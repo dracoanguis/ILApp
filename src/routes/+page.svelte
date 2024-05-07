81ae6794fc99839c24f9d52d34b7e2de1cb4fcdd
ethan <ethanbargaudarm@gmail.com>
2024-05-07 16:46:31 +0200

    Added basic dependcies (flowbite; threalte)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Canvas, T } from "@threlte/core";
+    import { Navbar } from "flowbite-svelte";
+
+</script>
+
+<Navbar>
+</Navbar>
+
+<Canvas>
+    <T.BoxGeometry>
+
+    </T.BoxGeometry>
+</Canvas>
