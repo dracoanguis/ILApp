b5f9487a9c75529965b0e42f31c31b3767265ed6
Aylin <aylin.tekkoyun@etu.unige.ch>
2024-05-18 14:15:15 +0200

    Add male model

--- src/lib/model.svelte
+++ src/lib/model.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
-    import { Canvas, T} from "@threlte/core";
+    import { Canvas, T } from "@threlte/core";
     import { GLTF, Grid, Gizmo, OrbitControls } from "@threlte/extras";
+    import { onDestroy } from 'svelte';
+    import { get } from 'svelte/store';
+    import type { Writable } from 'svelte/store';
+
+    export let model: Writable<number>;
+
+    let model_path = ["/female_base_mesh/scene.gltf", "/male_base_mesh/scene.gltf"];
+    let modelIndex = 0; 
+
+    // Subscribe to the model store
+    const unsubscribe = model.subscribe(value => {
+        modelIndex = value;
+    });
+
+    // Cleanup on component destroy
+    onDestroy(() => {
+        unsubscribe();
+    });
 </script>
 
 <Gizmo />
@@ -11,9 +29,9 @@
   target={[0, 5, 0]}
   zoom={2.5}
 >
-  <OrbitControls autoRotate enableDamping/>
+  <OrbitControls autoRotate enableDamping />
 </T.PerspectiveCamera>
-<T.AmbientLight intensity={3} />
-<T.DirectionalLight position={[4, 10, 4]} color={"blue"} intensity={20}/>
-<Grid type={"polar"} maxRadius={3}/>
-<GLTF url={"/female_base_mesh/scene.gltf"} castShadow/>
+<T.AmbientLight intensity={3} color={"lightBlue"} />
+<T.DirectionalLight position={[4, 10, 4]} color={"lightBlue"} intensity={20} />
+<Grid type={"polar"} maxRadius={3} />
+<GLTF url="{model_path[modelIndex]}" castShadow />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,26 @@
     import Chat from "$lib/chat.svelte";
     import { Canvas } from "@threlte/core";
     import Model from "$lib/model.svelte";
+    import { ButtonGroup, GradientButton } from 'flowbite-svelte';
+    import { writable } from 'svelte/store';
+
+    // Create a writable store to hold the model value
+    let model = writable(0);
+
+    // Function to handle button clicks
+    function setModel(value: number) {
+        model.set(value);
+    }
 </script>
 
 <div class="flex justify-stretch h-full bg-slate-100">
     <div class="half">
+        <ButtonGroup class="m-8">
+            <GradientButton color="purpleToBlue" on:click={() => setModel(0)}>Femme</GradientButton>
+            <GradientButton color="cyanToBlue" on:click={() => setModel(1)}>Homme</GradientButton>
+        </ButtonGroup>
         <Canvas>
-            <Model/>
+            <Model {model}/>
         </Canvas>
     </div>
     <div class="half flex justify-center">
