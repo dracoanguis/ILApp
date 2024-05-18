0b2ab3c6e1f05d12bbf80b6076df8694d5468e70
Aylin <aylin.tekkoyun@etu.unige.ch>
2024-05-18 12:21:48 +0200

    Separating model and chat

---
+++ src/lib/model.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { Canvas, T} from "@threlte/core";
+    import { GLTF, Grid, Gizmo, OrbitControls } from "@threlte/extras";
+</script>
+
+<Gizmo />
+<T.PerspectiveCamera
+  makeDefault
+  position={[-2, 10, -20]}
+  fov={36}
+  target={[0, 5, 0]}
+  zoom={2.5}
+>
+  <OrbitControls autoRotate enableDamping/>
+</T.PerspectiveCamera>
+<T.AmbientLight intensity={3} />
+<T.DirectionalLight position={[4, 10, 4]} color={"blue"} intensity={20}/>
+<Grid type={"polar"} maxRadius={3}/>
+<GLTF url={"/female_base_mesh/scene.gltf"} castShadow/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,13 @@
 <script lang="ts">
     import Chat from "$lib/chat.svelte";
-    import { Canvas, T} from "@threlte/core";
-    import { GLTF, Grid, Gizmo, OrbitControls } from "@threlte/extras";
+    import { Canvas } from "@threlte/core";
+    import Model from "$lib/model.svelte";
 </script>
 
 <div class="flex justify-stretch h-full bg-slate-100">
     <div class="half">
         <Canvas>
-            <Gizmo />
-            <T.PerspectiveCamera
-              makeDefault
-              position={[-2, 10, -20]}
-              fov={36}
-              target={[0, 5, 0]}
-              zoom={2.5}
-            >
-              <OrbitControls autoRotate enableDamping/>
-            </T.PerspectiveCamera>
-            <T.AmbientLight intensity={2} />
-            <T.DirectionalLight position={[1, 10, 1]} color={"blue"} intensity={20} />
-            <Grid type={"polar"} maxRadius={3}/>
-            <GLTF url={"/female_base_mesh/scene.gltf"} castShadow/>
+            <Model/>
         </Canvas>
     </div>
     <div class="half flex justify-center">
