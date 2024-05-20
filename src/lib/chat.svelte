5c0793cdb49b13d4156ff5edde1a526a654c7fd0
ethan <ethanbargaudarm@gmail.com>
2024-05-20 18:49:47 +0200

    Added the joke about send button

--- src/lib/chat.svelte
+++ src/lib/chat.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Card, GradientButton, Input } from "flowbite-svelte";
+    import { Card, GradientButton, Input, Toast } from "flowbite-svelte";
 
     let message_list: [number,string][] = [
         [0, "Hello, I’m IvaBot! 👋 I’m your personal shopping assistant. How can I help you?"],
@@ -7,10 +7,12 @@
         [0, "Here are some colorful dresses available on our platform"]
     ];
 
+    let notif = false;
+
 </script>
 
 
-<Card class="shadow-md m-4 max-w-full flex-col justify-between">
+<Card class="shadow-md m-4 max-w-full flex-col justify-between relative">
     <div class="flex flex-col flex-wrap">
         {#each message_list as message}
             {#if message[0] == 0}
@@ -26,8 +28,14 @@
     </div>
     <div class="flex flex-row my-2 gap-5">
         <Input placeholder="Your message" color=""/>
-        <GradientButton shadow color="purple" pill>
+        <GradientButton shadow color="purple" pill on:click={() => notif=true}>
             Send
         </GradientButton>
     </div>
+    <Toast color="red" position="bottom-left" open={notif} on:close={() => notif=false}>
+        <svelte:fragment slot="icon">
+            <img src="/ivabot.svg" alt="sad face"/>
+        </svelte:fragment>
+        Not yet implemented
+    </Toast>
 </Card>
